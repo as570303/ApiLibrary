a5ab8cd65ffc3dc7bcc8da8957b4fb48befc5635
as5703013 <as752497576@gmail.com>
2023-08-07 17:45:50 +0800

    测试类

---
+++ app/src/main/java/com/common/apilibrary/Test.java
@@ -0,0 +1,18 @@
+package com.common.apilibrary;
+
+/**
+ * @author as752497576@gmail.com
+ * @description
+ * @time 2023/8/7
+ */
+public  class Test {
+
+
+  public static  String getString (){
+
+
+    return  "获取成功";
+  }
+
+
+}
